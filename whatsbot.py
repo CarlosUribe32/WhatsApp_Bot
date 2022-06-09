8fa97d5be216b515c6e94571d171aeb05b30f1f0
Candre <carlos.uribecupb.edu.co>
2022-06-09 12:08:04 -0500

    Seleeccion del chat

--- whatsbot.py
+++ whatsbot.py
@@ -4,9 +4,12 @@ from selenium import webdriver
 from selenium.webdriver.remote.webdriver import WebDriver as RemoteWebDriver
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.common.keys import Keys
+import threading
 
 driver = webdriver.Chrome(executable_path=r"C:\DriveChrome\chromedriver.exe")
 dicContactos ={}
+condicion = threading.Condition()
+hiloEnUso = False
 
 def abrirWhatsApp():
     driver.get("https://web.whatsapp.com/")
@@ -28,6 +31,33 @@ def autenticacionWhatsApp():
         if(esperar==False):
             print("Gracias admin por validar")
 
+def buscarContacto(contacto):
+    buscando = True
+    while (buscando):
+        elementos = driver.find_elements_by_tag_name("span")
+        for elemento in elementos:
+            if(elemento.get_attribute("title") == contacto):
+                elemento.click()
+                buscando = False
+                break
+
+
+def regulacionChat(contacto, condicion):
+    while(True):
+        with condicion:
+            #Esperar que otros hilos se ejecuten
+            # if(hiloEnUso==True):
+            #     condicion.wait()
+            #Pedir mi turno de ejecucion (hilo)
+            # hiloEnUso =True
+            buscarContacto(contacto)
+            # hiloEnUso = False
+            #condicion.notifyAll()
+
+
+
+
+
 
 
 def main():
@@ -43,8 +73,8 @@ def main():
         name = input()
         if(name=="F"):
             break
-        dicContactos.setdefault(name, 'Hola')
-        print(dicContactos)
+        hilo = threading.Thread(name="hilo%s" %name, target=regulacionChat, args=(name, condicion))
+        dicContactos.setdefault(name, hilo.start())
 
 if __name__ == '__main__':
     main()
